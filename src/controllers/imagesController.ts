b88fd9555c3994405a970ee11353950f5b18d143
Damian X <dx4336969@gmail.com>
2024-12-18 09:22:04 +0000

    implementing sorting

--- src/controllers/imagesController.ts
+++ src/controllers/imagesController.ts
@@ -1,25 +1,29 @@
-import { Request, Response } from 'express'
-import User from '../models/userModel'
+import { Request, Response } from "express";
+import User from "../models/userModel";
 
-export async function getAllImages(req:Request,res:Response){
-    const queryObj = {...req.query}
-    const excludedFields = ['field','page','sort','limit']
-    excludedFields.forEach(el=> delete queryObj[el])
-    let query;
-    let queryStr = JSON.stringify(queryObj)
-    console.log(queryStr)
-    console.log(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match=>`$${match}`)
-    console.log('querystr',JSON.parse(queryStr))
-    
-    query =  User.find(JSON.parse(queryStr))
-    const users = await query
-    res.status(200).json({
-        status: 'success',
-        data: {
-            users
-        }
-    })
-    
-    
+export async function getAllImages(req: Request, res: Response) {
+  const queryObj = { ...req.query };
+  const excludedFields = ["field", "page", "sort", "limit"];
+  excludedFields.forEach((el) => delete queryObj[el]);
+  let query;
+  let queryStr = JSON.stringify(queryObj);
+  console.log(queryStr);
+  console.log(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  console.log("querystr", JSON.parse(queryStr));
+
+  query = User.find(JSON.parse(queryStr));
+  if (req.query.sort){
+    const sortField = typeof req.query.sort === "string" ? req.query.sort : null;
+      query = query.sort(sortField)
+
+  } 
+
+  const users = await query;
+  res.status(200).json({
+    status: "success",
+    data: {
+      users,
+    },
+  });
 }
