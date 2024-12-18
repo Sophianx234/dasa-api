6d41dfeb187fca3f54c7672b066c446be14176e2
Damian X <dx4336969@gmail.com>
2024-12-18 10:04:14 +0000

    implementing fields

--- src/controllers/imagesController.ts
+++ src/controllers/imagesController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
 
+export type reqQueryType = string|string[]|null
+
 export async function getAllImages(req: Request, res: Response) {
   const queryObj = { ...req.query };
   const excludedFields = ["field", "page", "sort", "limit"];
@@ -14,12 +16,21 @@ export async function getAllImages(req: Request, res: Response) {
 
   query = User.find(JSON.parse(queryStr));
   if (req.query.sort){
-    let sortField = typeof req.query.sort === "string" ? req.query.sort : null;
-    if(sortField?.includes(',')) sortField = sortField.replace(',',' ')
+    let sortField: reqQueryType = typeof req.query.sort === "string" ? req.query.sort : null;
+    if(sortField?.includes(',')) sortField = sortField.split(',').join(' ')
         console.log(sortField)
       query = query.sort(sortField)
 
   } 
+  if(req.query.field){
+    let field: reqQueryType = typeof req.query.field === 'string'? req.query.field : null;
+    
+    if (field?.includes(',')) field = field.split(',').join(' ')
+    if(field) query = query.select(field)
+        
+    // query = query.select(field)
+
+  }
 
   const users = await query;
   res.status(200).json({
