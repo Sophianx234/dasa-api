3687ad9f6f4840ba10f8e45592e3a4349548dcf3
Damian X <dx4336969@gmail.com>
2024-12-19 09:38:58 +0000

    adding implementing classes

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,7 @@ import { ApiFeatures } from "./ApiFeatures";
 export type reqQueryType = string|string[]|null
 
 export async function getAllUsers(req:Request,res:Response){
-  const features = new ApiFeatures(req.query,User.find())
+  const features = new ApiFeatures(req.query,User.find()).filter().sort().pagination()
   const users = await features.query
       res.status(200).json({
       status: "success",
