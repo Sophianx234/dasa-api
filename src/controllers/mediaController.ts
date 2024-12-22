3ac2be84be5db1b98669c034cf382b8a60ce82f5
Damian X <dx4336969@gmail.com>
2024-12-22 22:00:01 +0000

    implementing create media /media

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -46,29 +46,44 @@ export const getMedia = catchAsync(
   },
 );
 
+export const createMedia = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const feature = new ApiCRUD(req.body, Media);
+    const newMedia = await feature.create();
+    if (!newMedia) return next(new AppError("can't create new media", 404));
+    res.status(200).json({
+      status: "success",
+      data: {
+        newMedia,
+      },
+    });
+  },
+);
 export const updateMedia = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { fileUrl, fileType, fileSize } = req.body;
-    const {id} = req.params
-    const feature = new ApiCRUD({ fileUrl, fileType, fileSize },Media,id)
-    const media = await feature.update()
-    if(!media) return next(new AppError("can't find media with id",404))
-        res.status(200).json({
-    status: 'success',
-    data:{
-        media
-    }
-    })
+    const { id } = req.params;
+    const feature = new ApiCRUD({ fileUrl, fileType, fileSize }, Media, id);
+    const media = await feature.update();
+    if (!media) return next(new AppError("can't find media with id", 404));
+    res.status(200).json({
+      status: "success",
+      data: {
+        media,
+      },
+    });
   },
 );
 
-export const deleteMedia = catchAsync(async (req:Request,res:Response,next:NextFunction)=>{
-    const {id} = req.params
-    if(!id) return next(new AppError("can't find id:",404))
-    const feature = new ApiCRUD(req.body,Media,id)
-    await feature.delete()
+export const deleteMedia = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    if (!id) return next(new AppError("can't find id:", 404));
+    const feature = new ApiCRUD(req.body, Media, id);
+    await feature.delete();
     res.status(200).json({
-        status: 'success',
-        data: null
-    })
-})
+      status: "success",
+      data: null,
+    });
+  },
+);
--- src/routes/mediaRoute.ts
+++ src/routes/mediaRoute.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { deleteMedia, getAllMedia, getMedia, updateMedia } from "../controllers/mediaController";
+import { createMedia, deleteMedia, getAllMedia, getMedia, updateMedia } from "../controllers/mediaController";
 
 
 const router = Router();
 
-router.route('/').get(getAllMedia);
+router.route('/').get(getAllMedia).post(createMedia);
 router.route('/:id').get(getMedia).patch(updateMedia).delete(deleteMedia)
 
 
