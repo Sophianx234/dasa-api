cfd396fb66dec840c04f693f84a0689c638a99b2
Damian X <dx4336969@gmail.com>
2024-12-23 11:03:53 +0000

    implementing signup

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,8 +4,41 @@ import { ApiCRUD } from "../utils/ApiCRUD";
 import User from "../models/userModel";
 import { AppError } from "../utils/AppError";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import { NumberSchemaDefinition } from "mongoose";
+
+function signToken(next:NextFunction,user: any) {
+  const secret: string | undefined = process.env.JWT_SECRET;
+  const expires = process.env.JWT_EXPIRES_IN;
+  if (!secret) return next(new AppError("can't find jwt secret", 400));
+  return jwt.sign({ id: user.id }, secret, {
+    expiresIn: expires,
+  });
+}
+function createSendToken(user:any,statusCode:number,req:Request,res:Response,next:NextFunction) {
+  const token = signToken(next,user)
+  const cookieExpiry:number|undefined = Number(process.env.JWT_COOKIE_EXPIRES_IN)
+  if(!cookieExpiry) return next(new AppError('cookie expiry is not defined',400))
+  res.cookie('jwt',token,{
+    expires: new Date(
+      Date.now()+ cookieExpiry *24*60*60*1000
+
+    ),
+    httpOnly:true
+
+  })
+
+  user.password = null
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data:{
+      user
+    }
+  })
+
+
+
+}
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const {
@@ -33,19 +66,7 @@ export const signup = catchAsync(
     );
     const newUser = await feature.create();
     if (!newUser) return next(new AppError("can't create user ", 400));
-    const secret:string|undefined = process.env.JWT_SECRET;
-    if(!secret) return next(new AppError("can't find jwt secret",400))
-    
-    const token = jwt.sign({ id: newUser.id }, secret, {
-      expiresIn: "1h",
-    });
-    console.log(token)
-    res.status(200).json({
-      status: "success",
-      token,
-      data: {
-        newUser,
-      },
-    });
+
+    createSendToken(newUser,200,req,res,next)
   },
 );
