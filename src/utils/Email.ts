0814d435a63b873f476297f29956629ef84d5df0
Damian X <dx4336969@gmail.com>
2024-12-27 08:18:14 +0000

    implementing more nodemailer features

--- src/utils/Email.ts
+++ src/utils/Email.ts
@@ -1,8 +1,6 @@
 import { convert } from "html-to-text";
 import nodemailer from "nodemailer";
 import { userDocument } from "../models/userModel";
-import dotenv from 'dotenv'
-import { AppError } from "./AppError";
 
 export class Email {
   to: string;
@@ -29,13 +27,19 @@ export class Email {
 
   async send(subject: string) {
     const html = `
-    <div style="font-family: Arial, sans-serif; color: #4c4132; padding: 20px;">
-        <h1>Hello ${this.firstName}!</h1>
-        <p>You requested a password reset. Please click the link below to reset your password:</p>
-        <p><a href="${this.url}" style="color: #ffd8a8;">Click here to reset your password</a></p>
-        <p>The link is valid for only 10 minutes.</p>
-        <p>If you did not request this change, please ignore this email.</p>
-      </div>
+    <div style="font-family: Arial, sans-serif; color: #4c4132; padding: 20px; ">
+        <div style="display:flex; align-items:center ; transform: translate(-6%,10%) ; ">
+        
+        <img src="https://i.ibb.co/n8hRM6d/dasalogo-removebg.png" alt="dasalogo-removebg" width="150" />
+        <p style="font-weight: 800; border-left:solid 2px #4c4132; padding-block: 15px; padding-left: 8px; transform: translateX(-6%); ">Dagbon Students <br/>
+        Associantion</p>
+        </div>
+                <h1>Hello ${this.firstName}!</h1>
+                <p>You requested a password reset. Please click the link below to reset your password:</p>
+                <p><a href="${this.url}" style="color: #ffd8a8;">Click here to reset your password</a></p>
+                <p>The link is valid for only 10 minutes.</p>
+                <p>If you did not request this change, please ignore this email.</p>
+              </div>
     `;
     
 
