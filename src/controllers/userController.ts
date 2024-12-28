9158e825d422d5b5f70369e18983d171d01e4545
Damian X <dx4336969@gmail.com>
2024-12-28 17:06:25 +0000

    implementing image upload to imagebb

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,54 +3,81 @@ import User, { userDocument } from "../models/userModel";
 import { ApiFeatures } from "../utils/ApiFeatures";
 import { catchAsync } from "../utils/catchAsync";
 import { AppError } from "../utils/AppError";
-import multer, { FileFilterCallback } from 'multer'
+import multer, { FileFilterCallback } from "multer";
 import { RequestExtended } from "./authController";
-import sharp from 'sharp'
+import sharp from "sharp";
+import axios from "axios";
+import FormData from "form-data";
 export type reqQueryType = string | string[] | null;
-const multerStorage = multer.memoryStorage()
 
-const multerFilter =  function(req:RequestExtended,file:Express.Multer.File,cb:FileFilterCallback){
-  console.log(file)
-  req.file = file
-  if(file?.mimetype.startsWith('image')){
-    cb(null,true)
-  }else{
 
-    
-     cb(new AppError("can't upload file. please upload only images",400))
+
+const bufferToBlob = (buffer: Buffer, mimeType: string) => {
+  return new Blob([buffer], { type: mimeType });
+};
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = function (
+  req: RequestExtended,
+  file: Express.Multer.File,
+  cb: FileFilterCallback,
+) {
+  console.log(file);
+  req.file = file;
+  if (file?.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("can't upload file. please upload only images", 400));
   }
-
-}
+};
 
 const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
+});
 
-})
+export const uploadUserPhoto = upload.single("profileImage");
 
-export const uploadUserPhoto = upload.single('profileImage')
-
-export const resizeUserPhoto = catchAsync(async(req:RequestExtended,res:Response,next:NextFunction)=>{
-  if(!req.file) return next()
-    if(req.user){
-
-      req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
-      await sharp(req.file.buffer).resize(500,500,{
-        fit:'fill'
-      }).toFormat('jpeg').jpeg({quality:90}).toFile(`public/img/users/${req.file.filename}`)
+export const resizeUserPhoto = catchAsync(
+  async (req: RequestExtended, res: Response, next: NextFunction) => {
+    if (!req.file) return next();
+    if (req.user) {
+      req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+      const resizedBuffer = await sharp(req.file.buffer)
+        .resize(500, 500, {
+          fit: "fill",
+        })
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toBuffer();
+      const resizedBlob = bufferToBlob(resizedBuffer,'image/jpeg')
+      const formData = new FormData();
+      formData.append("image", resizedBlob, req.file.filename); // Append the image buffer to FormData
+      const api_key = process.env.API_KEY_IMAGE_BB;
+      const response = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${api_key}`,formData,{
+          headers: {
+            ...formData.getHeaders()
+          }
+        }
+      );
     }
-    next()
-})
+    next();
+  },
+);
 
-const filteredObj = function(obj: Record<string,any>,...allowedFields:string[]){
-  const newObj:Record<string,any> = {}
-  Object.keys(obj).forEach((el:any)=>{
-    if(allowedFields.includes(el)) newObj[el] = obj[el]
-  })
+const filteredObj = function (
+  obj: Record<string, any>,
+  ...allowedFields: string[]
+) {
+  const newObj: Record<string, any> = {};
+  Object.keys(obj).forEach((el: any) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
 
-  return newObj
-}
-
+  return newObj;
+};
 
 export const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -71,36 +98,47 @@ export const getAllUsers = catchAsync(
   },
 );
 
+export const deleteUser = catchAsync(
+  async (req: RequestExtended, res: Response, next: NextFunction) => {
+    req.params.id = req.user?.id;
+    const user = await User.findByIdAndUpdate(req.params.id, { active: false });
+    if (!user)
+      return next(new AppError("can't find user with specified id", 400));
+    res.status(400).json({
+      status: " success",
+      data: null,
+    });
+  },
+);
 
-export const deleteUser = catchAsync(async(req:RequestExtended,res:Response,next:NextFunction)=>{
-  req.params.id = req.user?.id
-  const user = await User.findByIdAndUpdate(req.params.id,{active:false})
-  if(!user) return next(new AppError("can't find user with specified id",400))
-  res.status(400).json({
-    status:' success',
-    data: null
-  })
+export const updateUser = catchAsync(
+  async (req: RequestExtended, res: Response, next: NextFunction) => {
+    const filteredBody = filteredObj(
+      req.body,
+      "name",
+      "hall",
+      "course",
+      "contact",
+    );
+    if (req.file) filteredBody.profileImage = req.file.filename;
+    console.log(filteredBody);
 
-
-})
-
-export const updateUser = catchAsync(async(req:RequestExtended,res:Response,next:NextFunction)=>{
-  const filteredBody = filteredObj(req.body,'name','hall','course','contact')
-  if(req.file) filteredBody.profileImage = req.file.filename
-  console.log(filteredBody)
-
-  const updatedUser = await User.findByIdAndUpdate(req.user?.id,filteredBody,{
-    new:true,
-    runValidators:true
-  })
-  if(!updatedUser) return next(new AppError("can't find user with that id",400))
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user?.id,
+      filteredBody,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!updatedUser)
+      return next(new AppError("can't find user with that id", 400));
 
     res.status(200).json({
-      status: 'success',
-      data:{
-        updatedUser
-      }
-    })
-
-})
-
+      status: "success",
+      data: {
+        updatedUser,
+      },
+    });
+  },
+);
