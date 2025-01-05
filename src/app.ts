549b07818e932e54ed4142ac7db5dfa50ad73bb5
Damian X <dx4336969@gmail.com>
2025-01-05 11:23:26 +0000

    disabling request limiter

--- src/app.ts
+++ src/app.ts
@@ -25,17 +25,17 @@ export type customError = Error & {
 };
 dotenv.config();
 const app = express();
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many request from this IP, please try again in an hour!",
-});
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many request from this IP, please try again in an hour!",
+// });
 
 app.use(helmet());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use("/api", limiter);
+// app.use("/api", limiter);
 app.use(compression())
 app.use(cors())
 app.use(express.json({
